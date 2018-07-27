604e1f33d6c24a4d59fae642d0f3885dfa837bfc
Manuel Perera <manuel.perera@innocv.com>
2018-07-27 16:13:41 +0200

    Change Glide for Picasso

--- app/src/main/java/com/manuelperera/topmovies20/domain/extensions/Extensions.kt
+++ app/src/main/java/com/manuelperera/topmovies20/domain/extensions/Extensions.kt
@@ -1,53 +1,49 @@
 package com.manuelperera.topmovies20.domain.extensions
 
-import android.graphics.drawable.Drawable
-import androidx.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import androidx.annotation.LayoutRes
+import com.manuelperera.topmovies20.R
 import com.mikhaellopez.circularimageview.CircularImageView
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 
-fun ImageView.loadImage(path: String, onExceptionDelegate: () -> Unit = {}, onResourceReadyDelegate: () -> Unit = {}) {
-    Glide.with(this.context)
+fun ImageView.loadImage(path: String, onErrorDelegate: () -> Unit = {}, onSuccessDelegate: () -> Unit = {}) {
+    Picasso
+            .get()
             .load(path)
-            .listener(object : RequestListener<Drawable> {
-                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                    onExceptionDelegate()
-                    return false
+            .placeholder(R.mipmap.ic_launcher_round)
+            .into(this, object : Callback {
+                override fun onSuccess() {
+                    onSuccessDelegate()
                 }
 
-                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                    onResourceReadyDelegate()
-                    return false
+                override fun onError(e: Exception?) {
+                    onErrorDelegate()
                 }
             })
-            .into(this)
 }
 
-fun CircularImageView.loadImage(path: String, onExceptionDelegate: () -> Unit = {}, onResourceReadyDelegate: () -> Unit = {}) {
-    Glide.with(this.context)
+fun CircularImageView.loadImage(path: String, onErrorDelegate: () -> Unit = {}, onSuccessDelegate: () -> Unit = {}) {
+    Picasso
+            .get()
             .load(path)
-            .listener(object : RequestListener<Drawable> {
-                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                    onExceptionDelegate()
-                    return false
+            .placeholder(R.mipmap.ic_launcher_round)
+            .into(this, object : Callback {
+                override fun onSuccess() {
+                    onSuccessDelegate()
                 }
 
-                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                    onResourceReadyDelegate()
-                    return false
+                override fun onError(e: Exception?) {
+                    onErrorDelegate()
                 }
             })
-            .into(this)
 }
 
 fun View.hide() {
